0c06da8b82164f56c1c5240d49a33365fab1a686
George Moschovitis <gmosx@reizu.com>
2022-12-21 16:32:01 +0200

    Add a scaffold of a repl

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,83 @@
-use clap::{Arg, Command};
+use clap::{Arg, ArgMatches, Command};
+use rustyline::{error::ReadlineError, Editor};
 use tan::{lexer::Lexer, parser::Parser};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-fn repl() {}
+// #TODO properly implement this.
+fn run(run_matches: &ArgMatches) -> anyhow::Result<()> {
+    let path: &String = run_matches
+        .get_one("PATH")
+        .expect("missing path to program file");
+
+    let input = std::fs::read_to_string(path).expect("cannot read input");
+
+    let mut lexer = Lexer::new(&input);
+    let tokens = lexer.lex().expect("cannot lex");
+    dbg!(&tokens);
+
+    let mut parser = Parser::new(tokens);
+    let expr = parser.parse();
+    dbg!(&expr);
+
+    Ok(())
+}
+
+// #TODO rename to `shell` or something else?
+fn repl() -> anyhow::Result<()> {
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new()?;
+
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+
+    loop {
+        // #TODO what would be a cool prompt?
+        // #TODO try to use the legendary `READY` in some capacity.
+        let readline = rl.readline(">> ");
+
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+
+                let mut lexer = Lexer::new(&line);
+                let tokens = lexer.lex();
+
+                let Ok(tokens) = tokens else {
+                    println!("Parse error: {}", tokens.unwrap_err());
+                    continue;
+                };
+
+                let mut parser = Parser::new(tokens);
+                let expr = parser.parse();
+
+                let Ok(expr) = expr else {
+                    println!("Parse error: {}", expr.unwrap_err());
+                    continue;
+                };
+
+                println!("{expr:?}");
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    rl.save_history("history.txt").unwrap();
+
+    Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
     // #TODO consider a different name? even though 'run' is generic enough.
@@ -24,21 +98,9 @@ fn main() -> anyhow::Result<()> {
     let matches = tan_cmd.get_matches();
 
     if let Some(run_matches) = matches.subcommand_matches("run") {
-        let path: &String = run_matches
-            .get_one("PATH")
-            .expect("missing path to program file");
-
-        let input = std::fs::read_to_string(path).expect("cannot read input");
-
-        let mut lexer = Lexer::new(&input);
-        let tokens = lexer.lex().expect("cannot lex");
-        dbg!(&tokens);
-
-        let mut parser = Parser::new(tokens);
-        let expr = parser.parse();
-        dbg!(&expr);
+        run(run_matches)?;
     } else {
-        // #TODO repl!
+        repl()?;
     }
 
     Ok(())
