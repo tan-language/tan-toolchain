6fd634e9adb8ed7f9635c2d1d780f3d9d3db79bc
George Moschovitis <gmosx@reizu.com>
2023-01-02 20:16:15 +0200

    Add placeholder lint subcommand

---
+++ src/lint.rs
@@ -0,0 +1,5 @@
+use clap::ArgMatches;
+
+pub fn handle_lint(_lint_matches: &ArgMatches) -> anyhow::Result<()> {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+mod lint;
 mod repl;
 mod run;
 mod util;
 
 use clap::{Arg, Command};
+use lint::handle_lint;
 
 use crate::repl::handle_repl;
 use crate::run::handle_run;
@@ -18,18 +20,28 @@ fn main() -> anyhow::Result<()> {
             .index(1),
     );
 
+    let lint_cmd = Command::new("lint").about("Lint a Tan text file").arg(
+        Arg::new("PATH")
+            .help("The path of the text")
+            .required(true)
+            .index(1),
+    );
+
     let tan_cmd = Command::new("tan")
         .bin_name("tan")
         .author("George Moschovitis, gmosx@reizu.org")
         .version(VERSION)
-        .about("A CLI for Tan")
-        .subcommand(run_cmd);
+        .about("A CLI for the Tan Language")
+        .subcommand(run_cmd)
+        .subcommand(lint_cmd);
 
     let matches = tan_cmd.get_matches();
 
     if let Some(run_matches) = matches.subcommand_matches("run") {
         // #TODO also handle run if a single `.tan` filename is passed.
         handle_run(run_matches)?;
+    } else if let Some(lint_matches) = matches.subcommand_matches("lint") {
+        handle_lint(lint_matches)?;
     } else {
         handle_repl()?;
     }
