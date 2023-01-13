902b03a1f0ff3f8ff355288b6f9129736fe85bf8
George Moschovitis <gmosx@reizu.com>
2023-01-13 21:22:32 +0200

    Refactor

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use tan::{api::eval_string, error::Error, eval::env::Env, expr::Expr};
+use tan::{api::eval_string, eval::env::Env, expr::Expr};
 use tan_fmt::format_error_pretty;
 
 pub fn eval_string_with_error_report(input: &str, env: &mut Env) -> Option<Expr> {
@@ -6,16 +6,8 @@ pub fn eval_string_with_error_report(input: &str, env: &mut Env) -> Option<Expr>
 
     match result {
         Ok(expr) => Some(expr),
-        Err(Error::Lexical(err)) => {
+        Err(err) => {
             eprintln!("{}", format_error_pretty(&err, input, None));
-            None
-        }
-        Err(Error::Parse(err)) => {
-            eprintln!("{}", format_error_pretty(&err, input, None));
-            None
-        }
-        Err(Error::Eval(err)) => {
-            eprintln!("{err}");
             None
         }
     }
