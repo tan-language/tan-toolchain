cfa96cedc4f7d84d0e86ba3b19a9ca4b5e6abe78
George Moschovitis <gmosx@reizu.com>
2023-01-16 22:16:42 +0200

    Minor

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,11 @@
-use tan::{api::eval_string, eval::env::Env, expr::Expr};
+use tan::{ann::Ann, api::eval_string, eval::env::Env, expr::Expr};
 use tan_fmt::format_error_pretty;
 
 pub fn eval_string_with_error_report(input: &str, env: &mut Env) -> Option<Expr> {
     let result = eval_string(input, env);
 
     match result {
-        Ok(expr) => Some(expr),
+        Ok(Ann(expr, ..)) => Some(expr),
         Err(err) => {
             eprintln!("ERROR: {}", format_error_pretty(&err, input, None));
             None
