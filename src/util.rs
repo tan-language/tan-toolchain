022d99ae3b365746315e56b80cf7060ead3d5b97
George Moschovitis <gmosx@reizu.com>
2023-06-17 10:39:46 +0300

    Update to latest error infra

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,4 @@
-use tan::{ann::Ann, api::eval_string, error::Error, eval::env::Env, expr::Expr, range::Ranged};
+use tan::{ann::Ann, api::eval_string, error::Error, eval::env::Env, expr::Expr};
 use tan_fmt::format_error_pretty;
 
 #[allow(dead_code)]
@@ -8,31 +8,18 @@ pub fn eval_string_with_error_report(input: &str, env: &mut Env) -> Option<Expr>
     match result {
         Ok(Ann(expr, ..)) => Some(expr),
         Err(errors) => {
-            // #TODO extract this as a method.
-            let mut error_strings = Vec::new();
-            for error in errors {
-                error_strings.push(format!(
-                    "ERROR: {}",
-                    format_error_pretty(&error, input, None)
-                ));
-            }
-            eprintln!("{}", error_strings.join("\n\n"));
+            eprintln!("{}", format_errors(&errors));
             None
         }
     }
 }
 
 #[allow(dead_code)]
-pub fn format_errors(errors: &[Ranged<Error>]) -> String {
+pub fn format_errors(errors: &[Error]) -> String {
     let mut error_strings = Vec::new();
 
-    // #TODO how to format the errors here?
     for error in errors {
-        error_strings.push(format!(
-            "ERROR: {}",
-            error,
-            // format_error_pretty(&error, input, None)
-        ));
+        error_strings.push(format!("ERROR: {}", format_error_pretty(&error)));
     }
 
     format!("{}", error_strings.join("\n\n"))
