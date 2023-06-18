7bb5022a742d7a890995cee4b957c13b5f6a5b9b
George Moschovitis <gmosx@reizu.com>
2023-06-18 12:57:21 +0300

    Improve error handling for use

--- src/run.rs
+++ src/run.rs
@@ -1,8 +1,11 @@
 use std::path::Path;
 
 use clap::ArgMatches;
-use tan::eval::{env::Env, util::eval_module};
-use tan_fmt::format_error_pretty;
+use tan::{
+    error::ErrorKind,
+    eval::{env::Env, util::eval_module},
+};
+use tan_fmt::{format_error, format_error_pretty};
 
 // #TODO try to reuse the code from "use".
 /// Read and evaluate a Tan program file.
@@ -13,14 +16,30 @@ pub fn handle_run(run_matches: &ArgMatches) -> anyhow::Result<()> {
 
     let mut env = Env::prelude();
     let path = Path::new(path);
+
     let result = eval_module(path, &mut env);
+
     if let Err(errors) = result {
         let mut error_strings = Vec::new();
 
         for error in errors {
-            // #TODO temp solution, can we optimize?
-            let input = std::fs::read_to_string(&error.file_path)?;
-            error_strings.push(format!("ERROR: {}", format_error_pretty(&error, &input)));
+            match error.kind() {
+                ErrorKind::FailedUse(_module_path, inner_errors) => {
+                    let mut strings = Vec::new();
+
+                    strings.push(format!("ERROR: {}", format_error(&error)));
+                    for inner_error in inner_errors {
+                        strings.push(format!("       + {}", format_error(&inner_error)));
+                    }
+
+                    error_strings.push(strings.join(""));
+                }
+                _ => {
+                    // #TODO temp solution, can we optimize?
+                    let input = std::fs::read_to_string(&error.file_path)?;
+                    error_strings.push(format!("ERROR: {}", format_error_pretty(&error, &input)));
+                }
+            }
         }
 
         // #TODO use tracing::info!()
