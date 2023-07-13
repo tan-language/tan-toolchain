ffb52dc3f0663e5c0a36607da79c6800b436f0a9
George Moschovitis <gmosx@reizu.com>
2023-07-13 20:01:37 +0300

    Add Format sub-command (#1)
    * Add format cmd
    
    * Minor refactor

--- src/format.rs
+++ src/format.rs
@@ -1,1 +1,22 @@
-// #TODO tan format command.
+use clap::ArgMatches;
+use tan::api::parse_string_all;
+use tan_formatting::pretty::Formatter;
+
+pub fn handle_format(lint_matches: &ArgMatches) -> anyhow::Result<()> {
+    let path: &String = lint_matches
+        .get_one("PATH")
+        .expect("missing path to program file");
+
+    let input = std::fs::read_to_string(path)?;
+
+    let Ok(exprs) = parse_string_all(&input) else {
+        return Err(anyhow::anyhow!("cannot parse the file"));
+    };
+
+    let formatter = Formatter::new(&exprs);
+    let formatted = formatter.format();
+
+    print!("{formatted}");
+
+    Ok(())
+}
