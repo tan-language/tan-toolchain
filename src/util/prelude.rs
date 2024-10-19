3f2e3b0acc4dd454ce94f0673bd26bdb1e5b62e4
George Moschovitis <george.moschovitis@gmail.com>
2024-10-19 19:36:49 +0300

    Minor.

--- src/util/prelude.rs
+++ src/util/prelude.rs
@@ -1,16 +1,12 @@
 // #todo Move to other crate.
 
 use tan::context::Context;
-use tancore::setup_lib;
 
 // #todo Find a better name for this function.
 pub fn import_prelude(context: &mut Context) {
-    setup_lib(context);
+    tancore::import_lib(context);
 
     // Setup prelude as the ultimate scope.
-
-    // let prelude_path = format!("{}/std/prelude", context.root_path);
-    // let prelude = context.module_registry.get(&prelude_path).unwrap();
 
     // #todo Just use `prelude` scope as top-level scope.
 
@@ -26,7 +22,6 @@ pub fn import_prelude(context: &mut Context) {
         context.top_scope.insert(name, value.clone());
     }
 
-    // #todo Nasty, temp hack, makes older api functions work, CLEANUP!
     // #todo We need scope-stack visualization.
     // #todo Do we really need this intermediate scope? for some reason this is needed! investigate why!
     // context.scope = Arc::new(Scope::new(context.top_scope.clone()));
